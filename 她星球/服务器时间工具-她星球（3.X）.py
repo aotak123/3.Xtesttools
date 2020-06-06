84cbc1f4889146bea372347f41f620c7ff461eff
aotak123 <ao.tak@163.com>
2020-06-06 13:15:12 +0800

    0606 优化代码，增加代码注释

--- 她星球/服务器时间工具-她星球（3.X）.py
+++ 她星球/服务器时间工具-她星球（3.X）.py
@@ -1,66 +1,60 @@
 # coding=utf-8
 import urllib.request
 import time
-                    ####################——————tak制作  vol.605——————####################
+                    ####################——————tak制作  vol.606——————####################
 
-url = "http://test-servertime.yiqibuduoduo.com/update_time/update?f=update"
+url = "http://test-servertime.yiqibuduoduo.com/update_time/update?f=update" #url请求地址
 
 while True:
     type = input("\n 请输入数字：\n 1：查询时间\n 2：恢复时间\n 3：修改时间\n")
 
     if type == "1":
         urls = "http://test-servertime.yiqibuduoduo.com/update_time/update?f=get"  # 疯狂猜成语获取服务器时间
-        request = urllib.request.Request(urls)
-        response = urllib.request.urlopen(request)
-        openurl = response.read()
-        timenow =openurl.decode()
-        print("\033[1;31m 当前服务时间： \033[0m")
+        request = urllib.request.Request(urls) #创建请求地址
+        response = urllib.request.urlopen(request) #请求打开地址
+        openurl = response.read() #读取页面内容
+        timenow =openurl.decode() #decode解密
+        print("\033[1;31m 当前服务时间： \033[0m") #打印时间
         print(timenow)
         # break
 
     if type == "2":
-        nowtime = time.strftime('%Y-%m-%d%%20%H:%M:%S')
-        fullurl2 = url + "&datetime=" + nowtime
-        # print(fullurl)
-        request = urllib.request.Request(fullurl2)
-        response = urllib.request.urlopen(request)
-        fullurl2bytes = response.read()
-        defullurl2 = fullurl2bytes.decode()
-        print("\033[1;31m 恢复成功 \033[0m")
+        nowtime = time.strftime('%Y-%m-%d%%20%H:%M:%S') #获取本地当前时间
+        fullurl2 = url + "&datetime=" + nowtime #拼接地址
+        # print(fullurl) #打印地址
+        request = urllib.request.Request(fullurl2)#创建请求地址
+        response = urllib.request.urlopen(request)#请求打开地址
+        fullurl2bytes = response.read()#读取页面内容
+        defullurl2 = fullurl2bytes.decode()#decode解密
+        print("\033[1;31m 恢复成功 \033[0m")#打印结果
         print(defullurl2)
         # break
 
     if type == "3":
         while True:  # 无限循环语句
-            nowtime = time.strftime('%Y-%m-%d%%20%H:%M:%S')
-            print("例子：" + nowtime)
+            nowtime = time.strftime('%Y-%m-%d%%20%H:%M:%S')#获取当前本地时间
+            print("例子：" + nowtime)#打印时间
             yeartime = input("请输入你想修改的时间：            \033[1;31m！！！请复制例子进行修改时间！！！\033[0m\n恢复时间请输入：0\n")
             #请使用例子中的格式，不得有误
             if yeartime == "0":
-                nowtime = time.strftime('%Y-%m-%d%%20%H:%M:%S')
-                nowtimeurl = url + "&datetime=" + nowtime
-                # print(nowtimeurl)
-                request = urllib.request.Request(nowtimeurl)
-                response = urllib.request.urlopen(request)
-                nowtimeurlbytes = response.read()
-                denowtimeurl = nowtimeurlbytes.decode()
+                nowtime = time.strftime('%Y-%m-%d%%20%H:%M:%S')#获取当前本地时间
+                nowtimeurl = url + "&datetime=" + nowtime #拼接地址
+                # print(nowtimeurl)#打印拼接地址
+                request = urllib.request.Request(nowtimeurl)#创建请求地址
+                response = urllib.request.urlopen(request)#请求打开地址
+                nowtimeurlbytes = response.read()#读取页面内容
+                denowtimeurl = nowtimeurlbytes.decode()#decode解密
                 print(denowtimeurl)
                 break
 
             else:
-                fullurl3 = url + "&datetime=" + yeartime
-                # print(fullurl3)
-                request = urllib.request.Request(fullurl3)
-                response = urllib.request.urlopen(request)
-                fullurl3bytes = response.read()
-                defullurl3 = fullurl3bytes.decode()
+                fullurl3 = url + "&datetime=" + yeartime #拼接请求地址
+                # print(fullurl3)#打印拼接地址
+                request = urllib.request.Request(fullurl3)#创建请求地址
+                response = urllib.request.urlopen(request)#请求打开地址
+                fullurl3bytes = response.read()#读取页面内容
+                defullurl3 = fullurl3bytes.decode()#decode解密
                 print(defullurl3)
 
-                    ####################——————tak制作  vol.605——————####################
+                    ####################——————tak制作  vol.606——————####################
                     ####################——————tak制作移植 2.X版——————####################
-
-"""
-Code producer：aotak
-Mobile：+86 18817762560
-E-Mail：hujunyi@021.com
-"""
--- 她星球/清理师徒关系-欢乐小秀才（3.X）.py
+++ 她星球/清理师徒关系-欢乐小秀才（3.X）.py
@@ -1,58 +1,40 @@
 # coding=utf-8
 import urllib.request
 import ssl
-                    ####################——————tak制作  vol.605——————####################
+                    ####################——————tak制作  vol.606——————####################
 
-while True:
-    a = input("\n 请输入获取的类型：\n 1：测试环境清除\n ***正式环境 请去Ocelot平台***\n ")
-
-    if a == "1":
-        url = "http://106.75.6.212:8020/testcleantools/cleanimei?apptypeid=100008&imei="
-    if a == "2":
-        # url = "https://invite.crazyccy.com/invite/Iiiii/imei_accid"
-        print("\033[1;31m 功能暂时无法使用！！！ \033[0m")
-        continue
-    elif a > "2":
-        print("\033[1;31m 输入有误，请重新选择！！！ \033[0m")
-        continue
-    elif a < "1":
-        print("\033[1;31m 输入有误，请重新选择！！！ \033[0m")
-        continue
-
-    while True:  # 无限循环语句
-        imei = input("请输入\033[1;31m imei\033[0m 或 \033[1;31moaid\033[0m 进行清除，返回请输入0\n")
-        # 添加快捷方式输入
-        if imei == "0":
-            break
+url = "http://106.75.6.212:8020/testcleantools/cleanimei?apptypeid=100008&imei="
+while True:  # 无限循环语句
+        imei = input("请输入\033[1;31m imei\033[0m 或 \033[1;31moaid\033[0m 进行清除\n")
+        # if imei == "0":
+        #     break #
         if imei == "":
             continue
-        if imei == "1":
+            """可在此添加设备信息以便快捷操作"""
+        if imei == "1":#iphone 7plus#
             imei = "9CFBCE03-BAE2-42DE-81CF-C537A1AD1D59"
-            deviceid = "F95F0A62-8C6E-40B5-ABE2-D855D49742F1"  # "#iphone 7plus
-
+            deviceid = "F95F0A62-8C6E-40B5-ABE2-D855D49742F1"
         if imei == "2":
-            # imei = "863056044142477"#huawei 8x
+            # imei = "863056044142477"#huawei 8x#
             imei = "7ff3e7df-b96e-e8e1-baf1-f6dcf75f2be3"
             deviceid = "94764aa49bb442e5"
             aaid = "461f14fe628616f42a085259e39cc978"
             oaid = "7ff3e7df-b96e-e8e1-baf1-f6dcf75f2be3"
-
-        if imei == "3":
-            # imei = "865973042769010"#黑鲨
-            imei = "43e219e4af34ad4e"#黑鲨
+        if imei == "3":#黑鲨#
+            # imei = "865973042769010"
+            imei = "43e219e4af34ad4e"
             deviceid = "43e219e4af34ad4e"
 
-
-
+        # 拼接地址
         fullurl = url + imei
         context = ssl._create_unverified_context()  # ssl证书免验证，python3需要验证https证书
         # print(fullurl)
 
         request = urllib.request.Request(fullurl)  # 构建请求url
-        response = urllib.request.urlopen(request)
-        num =response.read()
-        sss = num.decode()
-        print(imei +"  "+ sss)
+        response = urllib.request.urlopen(request)# 请求打开地址
+        num =response.read() # 读取页面内容
+        sss = num.decode()#内容decode解密
+        print(imei +"  "+ sss)##打印地址
 
                     ####################——————tak制作  vol.605——————####################
 """
