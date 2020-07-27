af23da0d993a30c58bb7d550e8ec8aaf46e0f244
aotak123 <ao.tak@163.com>
2020-07-27 18:45:43 +0800

    1.新增多多守护项目 2.新增跑日志

--- 自动化/一跑到底.py
+++ 自动化/一跑到底.py
@@ -4184,5 +4184,146 @@ print(BB)
 
 
 
+
+
+
+mobile = "18017700411"
+AA = "\033[1;31mNO.31\033[0m " + mobile + " \033[1;31m开始\033[0m"
+print(AA)
+nowtime = time.strftime('%Y-%m-%d% %H:%M:%S')
+print(nowtime)
+time.sleep(5)
+
+
+def check_login():
+    try:
+        check_login = driver.find_element_by_id(
+            "com.kamitu.drawsth.standalone.free.android:id/iv_mobile_login")  # 选择手机号登录
+    except NoSuchElementException:
+        print("自动登录成功")
+    else:
+        TouchAction(driver).tap(x=515, y=2300).perform()
+        time.sleep(2)
+        el2 = driver.find_element_by_id("com.kamitu.drawsth.standalone.free.android:id/edit_phone")  # 选择手机号输入框
+        el2.send_keys(mobile)
+        time.sleep(2)
+        TouchAction(driver).tap(x=1100, y=870).perform()  # 获取验证码
+        time.sleep(2)
+        # 获取验证码
+        logincodeurl = "https://uc.crazyccy.com/login/main_login/testtool?key=sLQq2_jaKLknsqAwZ&type=1&mobile=" + mobile
+        # print(logincodeurl)
+        request = urllib.request.Request(logincodeurl)  # 构建请求url
+        response = urllib.request.urlopen(request, context=context)  # ssl证书免验证加入,context = context# 打开请求url链接
+        num = response.read()  # 读取页面返回信息，python3返回数据为bytes类型的对象 (即b为前缀, bytes类型)
+        logincode = num.decode()
+        if logincode == "查不到！":
+            print("获取验证码失败")
+            TouchAction(driver).tap(x=1100, y=870).perform()  # 获取验证码
+        time.sleep(1)
+        el4 = driver.find_element_by_id("com.kamitu.drawsth.standalone.free.android:id/edit_identifyCode")  # 选择验证码输入框
+        time.sleep(2)
+        el4.send_keys(logincode)
+        time.sleep(2)
+        TouchAction(driver).tap(x=700, y=1200).perform()  # 确定按钮登录
+        print("账号登录成功")
+
+
+check_login()
+time.sleep(15)  # 等待10秒加载进入首页
+
+
+def check_signwindows():  # 检查用户签到弹窗
+    try:
+        check_signwindows = driver.find_element_by_id("com.kamitu.drawsth.standalone.free.android:id/iv_signin_get")
+    except NoSuchElementException:
+        print("无签到弹窗")
+    else:
+        TouchAction(driver).tap(x=1270, y=655).perform()
+        time.sleep(3)
+
+
+check_signwindows()
+time.sleep(3)
+TouchAction(driver).tap(x=707, y=2070).perform()  # 进入接龙页面
+time.sleep(3)
+driver.back()
+time.sleep(3)
+
+
+def check_signtoast():  # 检查是否开启签到提示
+    try:
+        check_signtoast = driver.find_element_by_id(
+            "com.kamitu.drawsth.standalone.free.android:id/iv_checkin_reminder_close")
+    except NoSuchElementException:
+        print("无签到提示")
+    else:
+        TouchAction(driver).tap(x=1228, y=748).perform()
+
+
+check_signtoast()
+time.sleep(3)
+TouchAction(driver).tap(x=175, y=2385).perform()  # 进入看图猜成语
+time.sleep(3)
+# 广告次数
+a = 12
+while a > 0:
+    TouchAction(driver).tap(x=475, y=2280).perform()
+    time.sleep(1)
+    TouchAction(driver).tap(x=1105, y=2188).perform()
+    time.sleep(1)
+    TouchAction(driver).tap(x=800, y=2280).perform()  # 选择(位置)
+    time.sleep(3)
+    TouchAction(driver).tap(x=444, y=1560).perform()  # 选择左侧去除多余选项广告
+    print(a)
+    time.sleep(6)
+    try:
+        GDT_G = driver.find_element_by_id("com.kamitu.drawsth.standalone.free.android:xml/gdt_file_path")
+    except NoSuchElementException:
+        print("toutiaosdk广告")
+        time.sleep(40)
+        TouchAction(driver).tap(x=1285, y=139).perform()  # 关闭toutiao广告
+        print("toutiaosdk广告关闭")
+    else:
+        print("GDTsdk广告")
+        time.sleep(55)
+        driver.back()  # GDTsdk广告按钮
+        print("GDTsdk广告关闭")
+    a -= 1
+    time.sleep(2)
+    TouchAction(driver).tap(x=1000, y=1560).perform()  # 选择右侧提示广告
+    print(a)
+    time.sleep(6)
+    try:
+        GDT_G = driver.find_element_by_id("com.kamitu.drawsth.standalone.free.android:xml/gdt_file_path")
+    except NoSuchElementException:
+        print("toutiaosdk广告")
+        time.sleep(40)
+        TouchAction(driver).tap(x=1285, y=139).perform()  # 关闭toutiao广告
+        print("toutiaosdk广告关闭")
+    else:
+        print("GDTsdk广告")
+        time.sleep(55)
+        driver.back()  # GDTsdk广告按钮
+        print("GDTsdk广告关闭")
+    # print(a)
+    time.sleep(2)
+    driver.back()
+    time.sleep(2)
+    a -= 1
+driver.back()  # 返回首页
+time.sleep(2)
+TouchAction(driver).tap(x=588, y=186).perform()  # 返回进入个人中心
+time.sleep(2)
+TouchAction(driver).tap(x=1275, y=243).perform()
+time.sleep(1)
+TouchAction(driver).tap(x=707, y=2467).perform()
+time.sleep(1)
+TouchAction(driver).tap(x=960, y=1440).perform()
+BB = mobile + " \033[1;31m账号退出成功\033[0m"
+print(BB)
+#######################################################################################################################
+
+
+
 nowtime = time.strftime('%Y-%m-%d% %H:%M:%S')
 print(nowtime)
