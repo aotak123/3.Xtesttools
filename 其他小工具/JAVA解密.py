788033f5b39b88959e8f5a7650629322a3c3c3eb
胡俊毅 <hujunyi@021.com>
2020-08-10 22:03:44 +0800

    增加代码循环

--- 其他小工具/JAVA解密.py
+++ 其他小工具/JAVA解密.py
@@ -4,9 +4,10 @@ import json
 
 s = requests.Session()
 url = "http://49.232.45.133/ToolWeb/journalDecode"
-type = input("\033[30m \n 请输入加密字符串\n")
-data1 = {
-        "bpStr": type
-    }
-json1 = (s.post(url, data=data1).json().get('decodeStr'))
-print(json1)
+while True:
+    type = input("\033[30m \n 请输入加密字符串\n")
+    data1 = {
+            "bpStr": type
+        }
+    json1 = (s.post(url, data=data1).json().get('decodeStr'))
+    print(json1)
