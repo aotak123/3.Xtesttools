1f43c639c8b00e074e1ea872f1a81bfc807283e5
胡俊毅 <hujunyi@021.com>
2020-08-10 20:14:44 +0800

    新增JAVA解密方法

---
+++ 其他小工具/JAVA解密.py
@@ -0,0 +1,11 @@
+# # coding=utf-8
+import requests
+import json
+s = requests.Session()
+url = "http://49.232.45.133/ToolWeb/journalDecode"
+type = input("\033[30m \n 请输入加密字符串\n")
+data1 = {
+        "bpStr": type
+    }
+json1 = (s.post(url, data=data1).json().get('decodeStr'))
+print(json1)
