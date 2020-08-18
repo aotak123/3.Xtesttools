e7e1837a4571cf5d4febd92a80664178fd16e110
胡俊毅 <hujunyi@021.com>
2020-08-18 17:48:04 +0800

    更新JAVA加密方式

--- 其他小工具/JAVA加密.py
+++ 其他小工具/JAVA加密.py
@@ -3,11 +3,25 @@ import requests
 import json
 
 s = requests.Session()
-url = "http://49.232.45.133/ToolWeb/encryptionDecode"
+url = "http://dc.021.com/strategic-hub/dispatch/ToolWeb/encryptionDecode"
+headers = {
+    'Host': 'dc.021.com',
+    'Accept': 'application/json, text/plain, */*',
+    'Authorization': '4D71BF611AEE857BB8F4F7C31774614F',
+    'Accept-Language': 'zh-cn',
+    'Accept-Encoding': 'gzip, deflate',
+    'Content-Type': 'application/json;charset=utf-8',
+    'Origin': 'http://dc.021.com',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
+    'Referer': 'http://dc.021.com/ocelot/',
+    'Content-Length': '2044',
+    'Cookie': 'OCELOT_USER_NAME=%E8%83%A1%E4%BF%8A%E6%AF%85; OCELOT_USER_TOKEN=4D71BF611AEE857BB8F4F7C31774614F; STRATEGICJSESSIONID=FB7D1804515281DCF4988E205725B1C0; userName=%E8%83%A1%E4%BF%8A%E6%AF%85',
+    'Connection': 'keep-alive',
+}
 while True:
-    type = input("\033[30m \n 请输入参数\n")
+    type = input("\033[30m \n 请输入加密字符串\n")
     data1 = {
             "bpStr": type
         }
-    json1 = (s.post(url, data=data1).json().get('decodeStr'))
+    json1 = (s.post(url, headers=headers,data=json.dumps(data1)).json().get('decodeStr'))
     print(json1)
