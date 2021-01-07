91b446a9e05ee8981ef84c13daa6b511d0eb20b3
aotak—macos <ao.tak@163.com>
2021-01-07 11:32:40 +0800

    增加时间

--- 广告/G9550run.py
+++ 广告/G9550run.py
@@ -108,9 +108,9 @@ def panguan(mobile):
             # print("     GDTsdk广告关闭")
         a += 1
     # 退出广告循环
-    time.sleep(8)
+    time.sleep(10)
     driver.back()  # 判官回到首页
-    time.sleep(2)
+    time.sleep(3)
     # 检查是否弹出签到提示
     # try:
     #     check_signtoast = driver.find_element_by_id(
