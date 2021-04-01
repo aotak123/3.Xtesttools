577f002fa818e3e6ec920da8989e72e047fa35ce
aotak—macos <ao.tak@163.com>
2021-04-01 14:16:42 +0800

    调整配置

--- 广告/G9550run.py
+++ 广告/G9550run.py
@@ -108,22 +108,22 @@ def panguan(mobile):
     while a <= 11:
         time.sleep(8)
         TouchAction(driver).tap(x=684, y=1857).perform()  # 点击查看广告
-        BB = "  \033[1;31mNO.\033[0m " + str(a) + " 次广告"
-        print(BB)
-        time.sleep(40)
-        TouchAction(driver).tap(x=1303, y=113).perform()  # 关闭toutiao广告
-        # try:
-        #     GDT = driver.find_element_by_id("com.kamitu.drawsth.standalone.free.android:xml/gdt_file_path")
-        # except NoSuchElementException:
-        #     BB = "  \033[1;31mNO.\033[0m " + str(a) + " toutiaosdk广告"
-        #     print(BB)
-        #     time.sleep(35)
-        #     TouchAction(driver).tap(x=1303, y=113).perform()  # 关闭toutiao广告
-        # else:
-        #     CC = "  \033[1;31mNO.\033[0m " + str(a) + " GDTsdk广告"
-        #     print(CC)
-        #     time.sleep(55)
-        #     driver.back()  # GDTsdk广告按钮
+        # BB = "  \033[1;31mNO.\033[0m " + str(a) + " 次广告"
+        # print(BB)
+        # time.sleep(40)
+        # TouchAction(driver).tap(x=1303, y=113).perform()  # 关闭toutiao广告
+        try:
+            GDT = driver.find_element_by_id("com.kamitu.drawsth.standalone.free.android:xml/gdt_file_path")
+        except NoSuchElementException:
+            BB = "  \033[1;31mNO.\033[0m " + str(a) + " toutiaosdk广告"
+            print(BB)
+            time.sleep(40)
+            TouchAction(driver).tap(x=1303, y=113).perform()  # 关闭toutiao广告
+        else:
+            CC = "  \033[1;31mNO.\033[0m " + str(a) + " GDTsdk广告"
+            print(CC)
+            time.sleep(55)
+            driver.back()  # GDTsdk广告按钮
         a += 1
     # 退出广告循环
     time.sleep(10)
