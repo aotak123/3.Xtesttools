f59b59536b046d9e00ee29ba1e0a27f7943154ab
aotak—macos <ao.tak@163.com>
2021-04-09 20:11:55 +0800

    -

--- 渠道打包/channelPKG.py
+++ 渠道打包/channelPKG.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 import os
 
-apk = "/Users/aotak/Desktop/shakeu_chinadouni_v1.0.0_2021-03-30-2241_Product.apk"  # 配置需要打包的母包地址，请使用『/』分隔符!!!
+apk = "/Users/aotak/Desktop/shakeu_chinadouni_v1.0.1_2021-04-09-1338_Test.apk"  # 配置需要打包的母包地址，请使用『/』分隔符!!!
 
 os.popen("java -jar walle-cli-all.jar batch2 -f config.json " + apk)
 print("打包成功，同母包目录下")
