a445f428bf405b60bb828e2de235e82078a6883d
aotak—macos <ao.tak@163.com>
2021-05-29 21:28:07 +0800

    新增支持360加固包打包

--- 渠道打包/channelPKG.py
+++ 渠道打包/channelPKG.py
@@ -1,7 +1,10 @@
 # -*- coding:utf-8 -*-
 import os
 
-apk = "/Users/aotak/Desktop/shakeu_chinadouni_v1.0.1_2021-04-09-1338_Test.apk"  # 配置需要打包的母包地址，请使用『/』分隔符!!!
+apk = "/Users/aotak/Desktop/crazyldinoms_v6.3.9_2021-05-29-2041_Product_639_jiagu_sign.apk"  # 配置需要打包的母包地址，请使用『/』分隔符!!!
 
-os.popen("java -jar walle-cli-all.jar batch2 -f config.json " + apk)
+# os.popen("java -jar walle-cli-all.jar batch2 -f config.json " + apk)  # 非加固包
+
+os.popen("java -jar new-walle-cli-all.jar batch2 -f config.json " + apk)  # 360加固包
+
 print("打包成功，同母包目录下")
