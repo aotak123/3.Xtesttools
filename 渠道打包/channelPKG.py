fb2c7c9d1d5974e4440528cd41a5f15a40924aca
aotak—macos <ao.tak@163.com>
2021-05-29 21:33:21 +0800

    1

--- 渠道打包/channelPKG.py
+++ 渠道打包/channelPKG.py
@@ -1,10 +1,12 @@
 # -*- coding:utf-8 -*-
 import os
 
-apk = "/Users/aotak/Desktop/crazyldinoms_v6.3.9_2021-05-29-2041_Product_639_jiagu_sign.apk"  # 配置需要打包的母包地址，请使用『/』分隔符!!!
+apk = "此处填写母包地址"  # 配置需要打包的母包地址，请使用『/』分隔符!!!
 
-# os.popen("java -jar walle-cli-all.jar batch2 -f config.json " + apk)  # 非加固包
+# 非加固包
+os.popen("java -jar walle-cli-all.jar batch2 -f config.json " + apk)
 
-os.popen("java -jar new-walle-cli-all.jar batch2 -f config.json " + apk)  # 360加固包
+# 360加固包
+os.popen("java -jar new-walle-cli-all.jar batch2 -f config.json " + apk)
 
 print("打包成功，同母包目录下")
