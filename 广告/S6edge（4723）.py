97b2c236270a18a3557ef3559aaadd3c9b117736
ao.tak <ao.tak@163.com>
2021-10-15 08:43:44 +0800

    qusia

--- 广告/S6edge（4723）.py
+++ 广告/S6edge（4723）.py
@@ -111,7 +111,7 @@ def panguan(mobile):
         time.sleep(5)
         BB = "  \033[1;31mNO.\033[0m " + str(a) + " 次广告"
         print(BB)
-        os.popen("adb -s 02157df2c8865716 shell am start -n com.kamitu.drawsth.standalone.free.android/com.qsmy.busniess.welcome.WelcomeActivity")
+        # os.popen("adb -s 02157df2c8865716 shell am start -n com.kamitu.drawsth.standalone.free.android/com.qsmy.busniess.welcome.WelcomeActivity")
         time.sleep(35)
         TouchAction(driver).tap(x=1285, y=139).perform()  # 关闭toutiao广告
         # time.sleep(5)
@@ -138,10 +138,10 @@ def panguan(mobile):
     try:
         set = driver.find_element_by_id("com.kamitu.drawsth.standalone.free.android:id/iv_setting")
     except NoSuchElementException:
-        os.popen("adb -s 02157df2c8865716 shell am force-stop com.kamitu.drawsth.standalone.free.android")
+        # os.popen("adb -s 02157df2c8865716 shell am force-stop com.kamitu.drawsth.standalone.free.android")
         time.sleep(2)
-        os.popen(
-            "adb -s 02157df2c8865716 shell am start -n com.kamitu.drawsth.standalone.free.android/com.qsmy.busniess.welcome.WelcomeActivity")
+        # os.popen(
+        #     "adb -s 02157df2c8865716 shell am start -n com.kamitu.drawsth.standalone.free.android/com.qsmy.busniess.welcome.WelcomeActivity")
         time.sleep(10)  # 等待加载进入首页
         TouchAction(driver).tap(x=588, y=186).perform()  # 返回进入个人中心
         time.sleep(2)
