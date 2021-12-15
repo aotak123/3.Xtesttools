456b47a889f198ef2cdfca084a5e873dafc89062
aotak—macos <ao.tak@163.com>
2021-12-15 09:46:38 +0800

    调整位置

--- 渠道打包/channelPKG.py
+++ 渠道打包/channelPKG.py
@@ -1,10 +1,10 @@
 # -*- coding:utf-8 -*-
 import os
 
-apk = "此处填写母包地址"  # 配置需要打包的母包地址，请使用『/』分隔符!!!
+apk = "/Users/aotak/Desktop/【抖你1.2.0版本基础包】chinaDouni_v1.2.0_2021-12-08-0014_Product_ceshi2.apk"  # 配置需要打包的母包地址，请使用『/』分隔符!!!
 
 # 非加固包
-os.popen("java -jar walle-cli-all.jar batch2 -f config.json " + apk)
+# os.popen("java -jar walle-cli-all.jar batch2 -f config.json " + apk)
 
 # 360加固包
 os.popen("java -jar new-walle-cli-all.jar batch2 -f config.json " + apk)
