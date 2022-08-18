be0cdec1fba94c075a1ac470709097f43e76478f
aotak—macos <ao.tak@163.com>
2022-08-18 18:28:58 +0800

    增加信息

--- 气泡资源文件点9图转换/RUN.py
+++ 气泡资源文件点9图转换/RUN.py
@@ -3,7 +3,7 @@ import os
 Tool = "/Users/aotak/PycharmProjects/3.Xtesttools/气泡资源文件点9图转换/dian9zhuanhuan"
 
 #替换图片地址后运行
-png = "/Users/aotak/Desktop/1.9.png"
+png = "/Users/aotak/Desktop/新人礼包气泡/1.png"
 
 os.popen("cd " + Tool + "&& aapt s -i " + png + " -o /Users/aotak/Desktop/run.png")
 
