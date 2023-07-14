19f57d3a819314ae7b293fb5a1a0b7b44b1e09b1
aotak—macos <ao.tak@163.com>
2023-07-14 17:22:32 +0800

    控制台错误信息打印支持

--- 气泡资源文件点9图转换/RUN.py
+++ 气泡资源文件点9图转换/RUN.py
@@ -3,8 +3,12 @@ import os
 Tool = "/Users/aotak/PycharmProjects/3.Xtesttools/气泡资源文件点9图转换/dian9zhuanhuan"
 
 #替换图片地址后运行
-png = "/Users/aotak/Desktop/1.9.png"
+png = "/Users/aotak/Desktop/1/1.9.png"
 
-os.popen("cd " + Tool + "&& aapt s -i " + png + " -o /Users/aotak/Desktop/run.png")
+cmd = 'ls -a'
+Call = os.popen("cd " + Tool + "&& aapt s -i " + png + " -o /Users/aotak/Desktop/run.png")
 
-print("文件存放在桌面")
+OutError = Call.read()
+
+print(OutError)
+
