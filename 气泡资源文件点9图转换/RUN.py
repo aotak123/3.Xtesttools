dc2fa38464c8707723d20f3beae1dea973b833d2
aotak—macos <ao.tak@163.com>
2023-07-24 17:41:03 +0800

    优化代码格式，增加换转文件命名

--- 气泡资源文件点9图转换/RUN.py
+++ 气泡资源文件点9图转换/RUN.py
@@ -1,14 +1,15 @@
 # -*- coding:utf-8 -*-
 import os
-Tool = "/Users/aotak/PycharmProjects/3.Xtesttools/气泡资源文件点9图转换/dian9zhuanhuan"
 
-#替换图片地址后运行
-png = "/Users/aotak/Desktop/1/1.9.png"
+Tool = "/Users/aotak/PycharmProjects/3.Xtesttools/气泡资源文件点9图转换/dian9zhuanhuan"  # 工具路径地址
 
-cmd = 'ls -a'
-Call = os.popen("cd " + Tool + "&& aapt s -i " + png + " -o /Users/aotak/Desktop/run.png")
+png_path = "/Users/aotak/Downloads/聊天气泡.9.png"  # 图片路径地址
 
-OutError = Call.read()
+png_rename = "run.png"  # 转换后文件命名
 
-print(OutError)
+cmd = 'ls -a'  # 前置cmd指令
+CallCMD = os.popen("cd " + Tool + "&& aapt s -i " + png_path + " -o /Users/aotak/Desktop/" + png_rename)  # cmd指令执行
 
+result = CallCMD.read()  # 读取cmd返回信息
+
+print(result)  # 打印cmd信息
