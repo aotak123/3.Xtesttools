cb77a6d032007abf96d7ca1faba2a270335ae384
aotak—macos <ao.tak@163.com>
2023-07-27 11:18:14 +0800

    1

--- 抢票/手动启动抢纷玩岛.py
+++ 抢票/手动启动抢纷玩岛.py
@@ -14,7 +14,7 @@ capabilities['newCommandTimeout'] = '600'
 driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', capabilities)  # 连接测试所在服务器
 
 # 配置
-starttime = 1690426800  # 请设置需要开抢的演唱会时间戳   2023-07-27 11:00:00
+starttime = 1690430400  # 请设置需要开抢的演唱会时间戳   2023-07-27 12:00:00
 """请手动启动纷玩岛app进入代抢页面并填写预约抢票后启动代码"""
 
 
@@ -30,27 +30,26 @@ def task():
             print('开始抢票')
             # TouchAction(driver).tap(x=698, y=2116).perform()  # 入口立即购买
             driver.tap([(215, 2040), (834, 2119)])  # 入口立即购买
-            time.sleep(0.3)
+            time.sleep(0.2)
             # TouchAction(driver).tap(x=698, y=2116).perform()  # 确认订单
             driver.tap([(673, 2081), (973, 2151)])  # 确认订单
-            time.sleep(0.3)
+            time.sleep(0.2)
             # TouchAction(driver).tap(x=698, y=2116).perform()  # 立即支付
             driver.tap([(673, 2081), (973, 2151)])  # 立即支付
-            time.sleep(1)
+            time.sleep(0.3)
             while True:
                 # TouchAction(driver).tap(x=531, y=1558).perform()  # 选择重试
                 driver.tap([(227, 1525), (805, 1601)])  # 选择重试
-                time.sleep(0.3)
+                time.sleep(0.2)
                 # TouchAction(driver).tap(x=698, y=2116).perform()  # 确认订单
                 driver.tap([(673, 2081), (973, 2151)])  # 确认订单
-                time.sleep(0.3)
+                time.sleep(0.2)
                 # TouchAction(driver).tap(x=698, y=2116).perform()  # 立即支付
                 driver.tap([(673, 2081), (973, 2151)])  # 立即支付
-                time.sleep(0.3)
+                time.sleep(0.2)
             # return
         elif nowtime < starttime:  # 如果现在时间＜开始时间
-            print("未到抢票时间")
-            print("需要等待时间：" + str(waitingtime))
+            print("未到抢票时间,需要等待：" + str(waitingtime))
             time.sleep(waitingtime)
 
 
