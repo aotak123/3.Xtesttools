07c2225c93333b558b8a2d39ea44a206b3475d15
aotak—macos <ao.tak@163.com>
2023-07-27 09:49:36 +0800

    去除中文命名，全部改用英文

--- 抢票/手动启动抢纷玩岛.py
+++ 抢票/手动启动抢纷玩岛.py
@@ -14,18 +14,19 @@ capabilities['newCommandTimeout'] = '600'
 driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', capabilities)  # 连接测试所在服务器
 
 # 配置
-演唱会开始时间 = 1690426800  # 请设置需要开抢的演唱会时间戳   2023-07-27 11:00:00
+starttime = 1690426800  # 请设置需要开抢的演唱会时间戳   2023-07-27 11:00:00
 """请手动启动纷玩岛app进入代抢页面并填写预约抢票后启动代码"""
+
 
 # 等待抢票
 def task():
     while True:
-        当前时间戳 = time.time()
-        当前时间 = int(当前时间戳)
-        需要等待时间 = 演唱会开始时间 - 当前时间
+        nowtimestamp = time.time()  # 当前时间戳
+        nowtime = int(nowtimestamp)  # 时间戳转换int格式
+        waitingtime = starttime - nowtime  # 需要等待的时间 = 开始时间 - 现在时间
 
         # print(当前时间)
-        if 当前时间 >= 演唱会开始时间:
+        if nowtime >= starttime:  # 如果 现在时间 ≥ 开始时间
             print('开始抢票')
             # TouchAction(driver).tap(x=698, y=2116).perform()  # 入口立即购买
             driver.tap([(215, 2040), (834, 2119)])  # 入口立即购买
@@ -47,10 +48,10 @@ def task():
                 driver.tap([(673, 2081), (973, 2151)])  # 立即支付
                 time.sleep(0.3)
             # return
-        elif 当前时间 < 演唱会开始时间:
+        elif nowtime < starttime:  # 如果现在时间＜开始时间
             print("未到抢票时间")
-            print("需要等待时间：" + str(需要等待时间))
-            time.sleep(需要等待时间)
+            print("需要等待时间：" + str(waitingtime))
+            time.sleep(waitingtime)
 
 
 task()
