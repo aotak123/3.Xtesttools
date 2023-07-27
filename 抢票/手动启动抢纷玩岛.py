c5108f4b4cb02b6414870b9579c76664f2cec06c
aotak—macos <ao.tak@163.com>
2023-07-27 14:01:34 +0800

    1

--- 抢票/手动启动抢纷玩岛.py
+++ 抢票/手动启动抢纷玩岛.py
@@ -49,8 +49,13 @@ def task():
                 time.sleep(0.1)
             # return
         elif nowtime < starttime:  # 如果现在时间＜开始时间
-            print("未到抢票时间,需要等待：" + str(waitingtime))
-            time.sleep(waitingtime)
+            if waitingtime > 180:  # 是否等待时间大于3分钟
+                print("未到抢票时间,需要等待：" + str(waitingtime))
+                print("3分钟后重新判断")
+                time.sleep(180)  # 等待3分钟后重新判断防止进程卡死
+            elif waitingtime <= 180:  # 是否等待时间小于3分钟
+                print("未到抢票时间,需要等待：" + str(waitingtime))
+                time.sleep(waitingtime)
 
 
 task()
