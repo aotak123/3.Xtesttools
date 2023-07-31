35754ae969e2708bbad108c98cfe81022b17e44c
aotak—macos <ao.tak@163.com>
2023-07-31 16:58:21 +0800

    改场次

--- 抢票/手动抢大麦.py
+++ 抢票/手动抢大麦.py
@@ -63,7 +63,7 @@ def task():
                 print("未到抢票时间,需要等待：" + str(waitingtime))
                 print("60秒后重新判断")
                 time.sleep(60)  # 等待1分钟后重新判断防止进程卡死
-                driver.tap([(44, 1977), (161, 1974)])  # 点击屏幕
+                # driver.tap([(44, 1977), (161, 1974)])  # 点击屏幕
             elif waitingtime <= 60:  # 是否等待时间小于1分钟
                 print("未到抢票时间,需要等待：" + str(waitingtime))
                 time.sleep(waitingtime - 0.5)
