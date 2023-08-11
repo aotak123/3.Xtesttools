23cea21bbde394578663d7a46c80d35d9b9d0c0a
aotak—macos <ao.tak@163.com>
2023-08-11 16:31:31 +0800

    改场次

--- 其他小工具/刷chainflix.py
+++ 其他小工具/刷chainflix.py
@@ -12,17 +12,16 @@ browser.get(url)
 browser.set_window_size(1320, 1000)
 time.sleep(5)
 print("等待加载完成")
-ActionChains(browser).move_by_offset(1038, 516).click().perform()  # 鼠标左键点击
-ActionChains(browser).move_by_offset(-1038, -516).perform()  # 将鼠标位置恢复到移动前
+ActionChains(browser).move_by_offset(920, 613).click().perform()  # 点击进入视频页
+# ActionChains(browser).move_by_offset(-1038, -516).perform()  # 将鼠标位置恢复到移动前
 time.sleep(5)
 print("进入播放页成功，可进行手动登录操作")
 """登录成功后请点击视频进行播放，后续则自动进入托管"""
 a = 1
 while a > 0:
-    print(" \033[1;31m当前NO.\033[0m " + str(a) + " 个视频")
+    print(" \033[1;31mNO.\033[0m "+ str(a) + " 个视频")
     time.sleep(300)
-    ActionChains(browser).move_by_offset(939, 734).click().perform()  # 鼠标左键点击
-    ActionChains(browser).move_by_offset(-939, -734).perform()  # 将鼠标位置恢复到移动前
+    ActionChains(browser).move_by_offset(0, 0).click().perform()  # 点击进入视频页
     a += 1
 
 # driver.find_element(By.XPATH, '//a[@class="ma-0 v-icon v-icon notranslate v-icon--is-component theme--light my-icon videoNext"]').click()
