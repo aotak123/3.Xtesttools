5f92d324fdf15923d1ad577e0477412b85eff3a5
aotak—macos <ao.tak@163.com>
2023-08-11 16:01:32 +0800

    改场次

--- 其他小工具/刷chainflix.py
+++ 其他小工具/刷chainflix.py
@@ -1,21 +1,24 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 import time
+
 # 在这里先打开一个浏览器的范围地址，设置为自动化打开模式。
 # 打开ChromeDriver下载地址：http://chromedriver.storage.googleapis.com/index.html
 url = 'https://www.chainflix.net/'
-# url = 'https://www.baidu.com/'
 # 然后再打开浏览器所在的地址
-driver = webdriver.Chrome()
+browser = webdriver.Chrome()
 # 最后打开浏览器，并访问设置的网址。
-driver.get(url)
+browser.get(url)
+browser.set_window_size(1320, 1000)
 time.sleep(5)
 print("等待加载完成")
-# element = driver.find_element_by_id('kw')
-# element.send_keys('白月黑羽')
-# driver.find_element_by_id('su').click()
-first_login = driver.find_element_by_xpath("/html/body/div[3]/div[1]/div[4]/span")
-first_login.click()
-
+ActionChains(browser).move_by_offset(1038, 516).click().perform()  # 鼠标左键点击
+ActionChains(browser).move_by_offset(-1038, -516).perform()  # 将鼠标位置恢复到移动前
+time.sleep(5)
+print("进入播放页")
+while True:
+    time.sleep(300)
+    ActionChains(browser).move_by_offset(939, 734).click().perform()  # 鼠标左键点击
+    ActionChains(browser).move_by_offset(-939, -734).perform()  # 将鼠标位置恢复到移动前
 
 # driver.find_element(By.XPATH, '//a[@class="ma-0 v-icon v-icon notranslate v-icon--is-component theme--light my-icon videoNext"]').click()
