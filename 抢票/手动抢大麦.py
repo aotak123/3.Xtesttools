aa55867f91b5a77cff836e1d22b52ae201e6b23f
aotak—macos <ao.tak@163.com>
2023-09-08 13:52:11 +0800

    改场次

--- 抢票/手动抢大麦.py
+++ 抢票/手动抢大麦.py
@@ -15,7 +15,7 @@ capabilities['newCommandTimeout'] = '600'
 driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', capabilities)  # 连接测试所在服务器
 
 # 配置
-starttime = 1693978200  # 请设置需要开抢的演唱会时间戳  2023-09-06 13:30:00
+starttime = 1694152800  # 请设置需要开抢的演唱会时间戳  2023-09-08 14:00:00
 
 """请手动启动大麦app进入演唱会待抢页面并填写预约抢票"""
 """请在抢票倒计时前5分钟左右开启,保证屏幕亮着"""
