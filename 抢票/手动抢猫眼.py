9fcb72d7a554c50400b30040998e589011ebe729
aotak—macos <ao.tak@163.com>
2023-09-27 14:43:35 +0800

    调整时间设置

--- 抢票/手动抢猫眼.py
+++ 抢票/手动抢猫眼.py
@@ -39,20 +39,8 @@ def task():
         # print(当前时间)
         if nowtime >= starttime:  # 如果 现在时间 ≥ 开始时间
             print('开始抢票')
-            driver.tap([(780, 2084), (982, 2151)])  # 入口立即购买
-            time.sleep(0.1)
-            driver.tap([(780, 2084), (982, 2151)])  # 确认订单
-            time.sleep(0.1)
-            driver.tap([(780, 2084), (982, 2151)])  # 立即支付
-            time.sleep(0.1)
             while True:
                 driver.tap([(297, 1456), (774, 1519)])  # 选择重试
-                time.sleep(0.1)
-                driver.tap([(79, 951), (477, 1355)])  # 选择档位
-                time.sleep(0.02)
-                driver.tap([(780, 2084), (982, 2151)])  # 确认订单
-                time.sleep(0.1)
-                driver.tap([(780, 2084), (982, 2151)])  # 立即支付
                 time.sleep(0.1)
             # return
         elif nowtime < starttime:  # 如果现在时间＜开始时间
@@ -64,21 +52,10 @@ def task():
                 print("未到抢票时间,需要等待：" + str(waitingtime))
                 time.sleep(waitingtime)
                 print('开始抢票')
-                driver.tap([(780, 2084), (982, 2151)])  # 入口立即购买
-                time.sleep(0.1)
-                driver.tap([(780, 2084), (982, 2151)])  # 确认订单
-                time.sleep(0.1)
-                driver.tap([(780, 2084), (982, 2151)])  # 立即支付
-                time.sleep(0.1)
                 while True:
-                    driver.tap([(297, 1456), (774, 1519)])  # 选择重试
+                    driver.tap([(780, 2079), (949, 2079)])
                     time.sleep(0.1)
-                    driver.tap([(79, 951), (477, 1355)])  # 选择档位
-                    time.sleep(0.02)
-                    driver.tap([(780, 2084), (982, 2151)])  # 确认订单
-                    time.sleep(0.1)
-                    driver.tap([(780, 2084), (982, 2151)])  # 立即支付
-                    time.sleep(0.1)
+
 
 
 task()
